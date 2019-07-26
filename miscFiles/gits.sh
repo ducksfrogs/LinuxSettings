36c6d85fb0d025e5e960c425d43396edb0d5520d
ma <ma_yamaki@yahoo.com>
2019-07-26 12:59:56 +0900

    momo

--- miscFiles/gits.sh
+++ miscFiles/gits.sh
@@ -4,4 +4,8 @@ cd ~/Documents
 
 mkdir gits &&cd gits
 
-git clone 
+git clone https://github.com/ducksfrogs/datacampPy.git
+git clone https://github.com/ducksfrogs/udemy_python.git
+git clone https://github.com/ducksfrogs/datacampR.git
+git clone https://github.com/ducksfrogs/masanariR.git
+git clone https://github.com/ducksfrogs/pycode.git
