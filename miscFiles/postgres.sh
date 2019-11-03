d08c14371e2629de52e300a2b7c016990fc3b1f0
ma <ma_yamaki@yahoo.com>
2019-11-03 13:43:39 +0900

    postgres

--- miscFiles/postgres.sh
+++ miscFiles/postgres.sh
@@ -4,11 +4,10 @@ sudo apt-get install curl ca-certificates gnupg
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 sudo cd /etc/apt/sources.list.d/
-sudo touch pgdg.list
 
 # echo 'deb http://apt.postgresql.org/pub/repos/apt bianic-pgdg main' >pgdg.list
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 
 sudo apt-get update
-sudo apt-get istall postgresql-11 pgadmin4
+sudo apt-get install postgresql-11 pgadmin4 -y
