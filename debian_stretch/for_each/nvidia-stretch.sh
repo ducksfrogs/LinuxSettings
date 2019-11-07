ddbdef0bc76a68e6e68b2f17a4296a883661fa1f
ma <ma_yamaki@yahoo.com>
2019-11-07 14:09:42 +0900

    mm

--- debian_stretch/for_each/nvidia-stretch.sh
+++ debian_stretch/for_each/nvidia-stretch.sh
@@ -1,3 +1,7 @@
 sudo aptitude update
 
 sudo aptitude -r install linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,') nvidia-driver
+
+sudo apt-get update
+
+sudo apt-get -t stretch-backports nvidia-driver
