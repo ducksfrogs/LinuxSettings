628615e5a51b69b681914bbe871a1587494fcf90
ma <ma_yamaki@yahoo.com>
2020-01-04 18:55:59 +0900

    libxml

--- ubuntu_settings/first.sh
+++ ubuntu_settings/first.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install zsh curl vim tmux git xclip ssh -y
+sudo apt-get install zsh curl vim tmux git xclip ssh  libxml2-dev -y
 
 cp settings/tmux.conf ~/.tmux.conf
 
