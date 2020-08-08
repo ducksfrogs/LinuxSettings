26d7d127dcefb56a8e80d408f7408a770f657718
ma <ma_yamaki@yahoo.com>
2020-08-08 18:55:36 +0900

    neovim

--- ubuntu_settings/postgres.sh
+++ ubuntu_settings/postgres.sh
@@ -4,7 +4,6 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install postgres pgadmin3 -y
 
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
