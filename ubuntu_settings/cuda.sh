c1b9552cfe8cd65ae7b9e89b1b2d5f63503bc3ce
ducksfrogs <ma_yamaki@yahoo.com>
2021-02-21 09:00:18 +0900

    dd

--- ubuntu_settings/cuda.sh
+++ ubuntu_settings/cuda.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 
-
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin -P ~/tmp
-cd ~/tmp/
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget https://developer.download.nvidia.com/compute/cuda/11.2.0/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.0-460.27.04-1_amd64.deb ~/tmp/
+wget https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda-repo-ubuntu2004-11-2-local_11.2.1-460.32.03-1_amd64.deb
 
-cd ~/tmp/
-sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.0-460.27.04-1_amd64.deb
-
+sudo dpkg -i cuda-repo-ubuntu2004-11-2-local_11.2.1-460.32.03-1_amd64.deb
 sudo apt-key add /var/cuda-repo-ubuntu2004-11-2-local/7fa2af80.pub
-sudo apt-get update
-
