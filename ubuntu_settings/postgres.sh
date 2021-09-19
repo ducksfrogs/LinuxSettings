1e171b721fca77a17050a77b2527022e91b509e7
ducksfrogs <ma_yamaki@yahoo.com>
2021-09-20 06:16:09 +0900

    dd

--- ubuntu_settings/postgres.sh
+++ ubuntu_settings/postgres.sh
@@ -4,14 +4,8 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql
-
-#pgadmin4
-
-sudo apt-get install curl ca-certificates gnupg -y
-curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get -y install postgresql
 
