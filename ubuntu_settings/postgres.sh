9143660cf37a559aa048d09bb7d0fb83a7f649ea
ducksfrogs <ma_yamaki@yahoo.com>
2021-09-20 12:31:06 +0900

    dd

--- ubuntu_settings/postgres.sh
+++ ubuntu_settings/postgres.sh
@@ -9,3 +9,12 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 sudo apt-get -y install postgresql
 
+# pgadmin4
+
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+sudo apt install pgadmin4
+
+
