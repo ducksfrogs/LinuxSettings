e9ea1de377c40e3c80bd13b36eb46b58c0c090f5
ducksfrogs <ma_yamaki@yahoo.com>
2021-09-23 15:40:09 +0900

    d

--- ubuntu_settings/postgres.sh
+++ ubuntu_settings/postgres.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 
-sudo apt-get update
-sudo apt-get upgrade -y
-
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y install postgresql
-
 # pgadmin4
 
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
