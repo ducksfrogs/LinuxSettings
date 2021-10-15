33cb1d19c45bb5dcd9912eb035e6cf42bafd4fff
ducksfrogs <ma_yamaki@yahoo.com>
2021-10-16 07:01:21 +0900

    d

---
+++ ubuntu_settings/wine.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo dpkg --add-architecture i386
+sudo apt-key add winehq.key
+sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+sudo apt update
+sudo apt install --install-recommends winehq-stable
+
