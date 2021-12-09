fea48731ea36e23ad14171a91c4b970eb7469018
ducksfrogs <ma_yamaki@yahoo.com>
2021-12-09 10:46:29 +0900

    d

--- centos_setting/first.sh
+++ centos_setting/first.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum -y update 
-sudo yum -y install vim tmux curl wget git bash-completion zsh
+sudo yum -y install vim tmux curl wget git bash-completion zsh util-linux-user
 
 cp settings/dotfiles/.tmux.conf ~/
 
