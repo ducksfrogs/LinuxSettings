98391e498edd7b322843ad4a263db0275a6118b1
ducksfrogs <ma_yamaki@yahoo.com>
2024-06-27 06:58:31 +0900

    first branck

--- ubuntu_settings/docker/docker_comp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-mkdir -p ~/.zsh/completion
-curl -L https://raw.githubusercontent.com/docker/compose/1.24.0/contrib/completion/zsh/_docker-compose > ~/.zsh/completion/_docker-compose
-
