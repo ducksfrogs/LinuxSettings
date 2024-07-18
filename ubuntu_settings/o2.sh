c872391bb3803a426896eb0abc9de80d81e70e85
ducksfrogs <ma_yamaki@yahoo.com>
2024-07-18 14:39:49 +0900

    d

--- ubuntu_settings/o2.sh
+++ ubuntu_settings/o2.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
-source ~/.zshrc
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+       	$ZSH_CUSTOM/plugins/zsh-vi-mode
