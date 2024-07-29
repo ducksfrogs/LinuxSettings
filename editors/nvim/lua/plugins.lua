ee595c1f0ad73b27ef0f326a3f0f039d6c2a36ff
ducksfrogs <ma_yamaki@yahoo.com>
2024-07-30 07:36:44 +0900

    ff

--- editors/nvim/lua/plugins.lua
+++ editors/nvim/lua/plugins.lua
@@ -1,10 +1,20 @@
 
 vim.cmd [[packadd packer.nvim ]]
 
-return require('packer').startup(function()
+return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'nvim-treesitter/nvim-treesitter'
     use {
 	'neoclide/coc.nvim', branch = 'release'
 	}
+
+    use 'rstacruz/vim-closer'
+
+    use {
+    "williamboman/mason.nvim"
+    }
+
+    use {
+        'ayu-theme/ayu-vim'
+    }
 end)
