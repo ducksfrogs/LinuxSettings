583ba118deb8ecd51c10ac7da535920428e3eaa9
ducksfrogs <ma_yamaki@yahoo.com>
2024-08-21 19:59:36 +0900

    vagra

--- ubuntu_settings/vi.sh
+++ ubuntu_settings/vi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib" > /etc/apt/sources.list
+sudo apt update
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
+sudo apt update
+sudo apt upgrade -y
+sudo apt install virtualbox-7.0
