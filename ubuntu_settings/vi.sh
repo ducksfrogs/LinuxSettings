e1f8d4e39fa9cf44af2d73edc93f8dee5666d638
ducksfrogs <ma_yamaki@yahoo.com>
2024-08-22 22:01:08 +0900

    ddd

--- ubuntu_settings/vi.sh
+++ ubuntu_settings/vi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian jammy contrib" > /etc/apt/sources.list
+sudo mv /etc/apt/sources.list /etc/apt/sourseces.list.bk
+sudo cp sources.list /etc/apt/
 sudo apt update
 wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
 sudo apt update
